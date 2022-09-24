0403ac2e9d1a7713dfc564d4b94b91d19b43c8c1
Eric Jacobson <contact@efjacobson.com>
2022-09-24 15:47:01 -0700

    yep, those are some files

---
+++ firmware/kira-firmware-v0.5.3-installer/flash.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$PWD/lib"
+export PATH="$PATH:$PWD/bin"
+
+DEVICE="308f:0012"
+FIRMWARE="kira.v0.5.3.dfu.bin"
+
+# Make sure dfu-util works
+dfu-util -V &>/dev/null
+if [ $? -ne 0 ]; then
+	echo "Please install dfu-util"
+	exit
+fi
+
+echo "Please plug in your keyboard and press the flash button now."
+echo
+
+echo -n "Waiting for Kira in flash mode..."
+while true; do
+	dfu-util -l | grep -q "${DEVICE}"
+	if [ $? -eq 0 ]; then
+		break
+	fi
+	echo -n "."
+	sleep 1
+done
+
+echo
+echo "Device Found!"
+echo
+sudo dfu-util -D "${FIRMWARE}"
